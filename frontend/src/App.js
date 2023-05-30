72d5fdbbbf8a2281762f15598b8d67b5c7fae292
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-30 15:09:58 -0700

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  const [gamesData, setGamesData] = useState('empty')
+  const [gameCards, setGameCards] = useState('empty')
+
+  useEffect(() => {
+    fetch('https://steam-games-server.onrender.com/')
+      .then(res => res.json())
+      .then(data => console.log('success!!', data))
+  }, [])
+
+  useEffect(() => {
+    if (gamesData !== 'empty') {
+      console.log(gamesData)
+      setGameCards(gamesData.map(game => {
+        const genreCards = game.genres.map(genre => {
+          return (
+            <div className='genre-tag' key={`${game.appId} + ${genre}`}>
+              {genre}
+            </div>
+          )
+        })
+        return (
+          <div key={game.appId} className='game-card'>
+            <h4>{game.name}</h4>
+            <img src={game.imgUrl} alt="" />
+            <div className='genre-cards-container'>
+              {genreCards}
+            </div>
+          </div>
+        )
+      }))
+    }
+  }, [gamesData])
+
+  
+  return (
+    <div className="App">
+      {gamesData === 'empty' ? <h1>...Loading</h1> : <h1>{gameCards}</h1>}
+    </div>
+  );
+}
+
+export default App;
