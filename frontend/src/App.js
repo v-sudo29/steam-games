14d6bde5449dc8897c6bf35c705d7ed0f89afc57
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-31 14:54:33 -0700

    Implement filter feature Abstract card to component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,77 @@
 import { useEffect, useState, useRef } from 'react'
+import Card from './components/Card';
 import './App.css';
 
 function App() {
-  const [gamesData, setGamesData] = useState('empty')
-  const [gameCards, setGameCards] = useState('empty')
+  const [gamesData, setGamesData] = useState(null)
+  const [gameCards, setGameCards] = useState(null)
+  const [filters, setFilters] = useState([])
   const searchValue = useRef(null)
+  const genreFilters = useRef([
+    'Base Building',
+    'Colony Sim',
+    'Farming',
+    'Farming Sim',
+    'Indie',
+    'Life Sim',
+    'Pixel Graphics',
+    '2D',
+  ])
 
+  // FUNCTION: HANDLE SEARCH BUTTON
   function handleSearch() {
     console.log(searchValue.current.value)
   }
 
+  // FUNCTION: HANDLE FILTER CLICK
+  function handleFilterClick(e) {
+    const currentFilter = e.target.innerHTML
+
+    if (e.target.classList.contains('filter-active')) {
+      e.target.classList.remove('filter-active')
+      setFilters(prevFilters => {
+        return prevFilters.filter(filter => filter !== currentFilter ? filter : null)
+      })
+    } else {
+      e.target.classList.add('filter-active')
+
+      if (filters.length === 0) {
+        setFilters([currentFilter])
+      } else {
+        setFilters(prevFilters => [...prevFilters, currentFilter])
+      }
+    }
+  }
+
+  // FETCH SERVER DATA
   useEffect(() => {
     fetch('https://steam-games-server.onrender.com/')
       .then(res => res.json())
       .then(data => {
-        setGamesData(data)
-        console.log(gamesData)
+
+        // Filter out data that don't have genres property
+        const newData = data.filter(game => game.genres ? game : null)
+        setGamesData(newData)
       })
   }, [])
 
+  // SET GENRE FILTER TAGS
+  const filterTags = genreFilters.current.map(genre => {
+    return (
+      <button 
+        key={`${genre}ref`} 
+        type='button'
+        className='filter-tag'
+        onClick={handleFilterClick}
+      >{genre}
+      </button>
+    )
+  })
+
+  // SET DEFAULT GAME CARDS
   useEffect(() => {
-    if (gamesData !== 'empty') {
+    if (gamesData !== null && filters.length === 0) {
       setGameCards(gamesData.map(game => {
-        if (!game.imgUrl) {
-          return null
-        }
         const gameGenres = game.genres ? game.genres : []
         const genreTags = gameGenres.map(genre => {
           return (
@@ -33,34 +80,70 @@ function App() {
             </div>
           )
         })
-
         return (
-          <div key={game.appId} className='game-card'>
-            <h4>{game.name}</h4>
-            <img src={game.imgUrl} alt="" />
-            <div className='price-info-container'>
-              <div className='discount-container'>Discount: <span className='discount-percentage'>{game.discount}</span></div>
-              <div className='original-price-container'>Original Price: <span className='original-price'>{game.originalPrice}</span></div>
-              <div className='current-price-container'>Current Price: <span className='current-price'>{game.currentPrice}</span></div>
-            </div>
-            <div className='genre-tags-container'>
-              {genreTags}
-            </div>
-          </div>
+          <Card
+            key={game.appId}
+            appId={game.appId}
+            name={game.name}
+            imgUrl={game.imgUrl}
+            discount={game.discount}
+            originalPrice={game.originalPrice}
+            currentPrice={game.currentPrice}
+            genreTags={genreTags}
+          />
         )
       }))
     }
-  }, [gamesData])
-
+  }, [gamesData, filters])
   
+  // SET NEW GAME CARDS
+  useEffect(() => {
+    if (filters.length !== 0 && gamesData !== null) {
+      const matchedGames = []
+
+      for (let i = 0; i < gamesData.length; i++) {
+        const genreExists = gamesData[i]['genres'].some(genre => filters.includes(genre))
+        if (genreExists) {
+          matchedGames.push(gamesData[i])
+        } 
+      }
+
+      setGameCards(matchedGames.map(game => {
+        const gameGenres = game.genres ? game.genres : []
+        const genreTags = gameGenres.map(genre => {
+          return (
+            <div key={`${game.appId+genre}`} className='genre-tag'>
+              {genre}
+            </div>
+          )
+        })
+        return (
+          <Card 
+            key={game.appId}
+            appId={game.appId}
+            name={game.name}
+            imgUrl={game.imgUrl}
+            discount={game.discount}
+            originalPrice={game.originalPrice}
+            currentPrice={game.currentPrice}
+            genreTags={genreTags}
+          />
+        )
+      }))
+    }
+  }, [filters])
+
   return (
     <div className="App">
       <div className='search-bar'>
         <input ref={searchValue} type="text" placeholder='Search'/>
         <button onClick={handleSearch} type="button">Search</button>
       </div>
+      <div className='genre-filter-container'>
+        {filterTags}
+      </div>
       <div className='search-results'>
-        {gamesData === 'empty' ? <h1>...Loading</h1> : <>{gameCards}</>}
+        {gamesData === null ? <h1>...Loading</h1> : <>{gameCards}</>}
       </div>
     </div>
   );
---
+++ frontend/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Card(props) {
+  return (
+    <div key={props.appId} className='game-card'>
+      <h3>{props.name}</h3>
+      <img src={props.imgUrl} alt="" />
+      <div className='price-info-container'>
+        <div className='discount-container'>Discount: <span className='discount-percentage'>{props.discount}</span></div>
+        <div className='original-price-container'>Original Price: <span className='original-price'>{props.originalPrice}</span></div>
+        <div className='current-price-container'>Current Price: <span className='current-price'>{props.currentPrice}</span></div>
+      </div>
+      <div className='genre-tags-container'>
+        {props.genreTags}
+      </div>
+    </div>
+  )
+}
+
+export default Card
