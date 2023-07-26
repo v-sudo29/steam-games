6732e18a0cd8c3cf7f2c0246dd17b1e42de4d20e
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-07-25 19:28:42 -0700

    Change scheduler

--- server/scheduler.js
+++ server/scheduler.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 
-const scheduleExpression = '56 * * * *' 
+const scheduleExpression = '30 19 * * *'
 
 async function updateWishlists() {
   const wishlistData = await wishlistScraper.run()
