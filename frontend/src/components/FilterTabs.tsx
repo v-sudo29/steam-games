8a62ca72642caa1c825a3d9d48d908549c493202
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-14 13:23:16 -0700

    Add conditional hover style to tabs

--- frontend/src/components/FilterTabs.tsx
+++ frontend/src/components/FilterTabs.tsx
@@ -6,8 +6,26 @@ import {
   TabList
 } from "@chakra-ui/react"
 import FilterIcon from "../assets/FilterIcon"
+import { useState } from "react"
 
 export default function FilterTabs() {
+  const [tabOneActive, setTabOneActive] = useState<boolean>(false)
+  const [tabTwoActive, setTabTwoActive] = useState<boolean>(true)
+  
+  const handleTabsChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const buttonElement = e.target as HTMLButtonElement
+    const tabName = buttonElement.innerText
+
+    if (tabName === 'All Games') {
+      setTabOneActive(true)
+      setTabTwoActive(false)
+    }
+    else {
+      setTabTwoActive(true)
+      setTabOneActive(false)
+    }
+  }
+
   return (
     <HStack gap='2rem'>
       <Button 
@@ -20,16 +38,20 @@ export default function FilterTabs() {
       </Button>
       <Tabs defaultIndex={1} variant='unstyled'>
         <TabList>
-          <Tab 
-            _selected={{ color: '#F5F5F5' }}
+          <Tab
+            onClick={(e) => handleTabsChange(e)}
+            _selected={tabOneActive ? { color: '#F5F5F5' } : { color: '#5C5F63' }}
+            _hover={!tabOneActive ? { color: 'whiteAlpha.700' } : { color: '#F5F5F5' }}
             color='#5C5F63'
             fontWeight='700'
             fontSize='1.3rem'
           >
             All Games
           </Tab>
-          <Tab 
-            _selected={{ color: '#F5F5F5' }}
+          <Tab
+            onClick={(e) => handleTabsChange(e)}
+            _selected={tabTwoActive ? { color: '#F5F5F5' } : { color: '#5C5F63' }}
+            _hover={!tabTwoActive ? { color: 'whiteAlpha.700' } : { color: '#F5F5F5' }}
             color='#5C5F63'
             fontWeight='700'
             fontSize='1.3rem'
