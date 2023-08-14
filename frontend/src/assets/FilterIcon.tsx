06fc54c5a537ee158f6d7df2de739e217ec34c2b
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-14 13:27:33 -0700

    Rename properties to align with JSX camelcase rule

--- frontend/src/assets/FilterIcon.tsx
+++ frontend/src/assets/FilterIcon.tsx
@@ -1,9 +1,9 @@
 export default function FilterIcon() {
   return (
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 12H6" stroke="#F5F5F5" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M21 7H3" stroke="#F5F5F5" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M15 17H9" stroke="#F5F5F5" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M18 12H6" stroke="#F5F5F5" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M21 7H3" stroke="#F5F5F5" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M15 17H9" stroke="#F5F5F5" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
 
   )
