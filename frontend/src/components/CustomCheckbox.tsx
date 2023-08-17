724846983c501e3242a879368cbdaa3ca01acafe
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-16 17:55:43 -0700

    Remove isSafari

--- frontend/src/components/CustomCheckbox.tsx
+++ frontend/src/components/CustomCheckbox.tsx
@@ -1,5 +1,4 @@
 import { Checkbox } from "@chakra-ui/react";
-import { isSafari } from "react-device-detect";
 import CheckboxIcon from "../assets/CheckboxIcon";
 
 export default function CustomCheckbox({ genre, handleGenreClick} : { genre: string, handleGenreClick: (e: React.ChangeEvent<HTMLInputElement>) => Promise<void> }) {
