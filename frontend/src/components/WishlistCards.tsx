d7b961c9bff3286e8c90b2b71ef331a4941a33f2
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-22 11:47:23 -0700

    Add default text if no wishlist cards found during filter

--- frontend/src/components/WishlistCards.tsx
+++ frontend/src/components/WishlistCards.tsx
@@ -136,7 +136,8 @@ export default function WishlistCards() {
   const twentyFiveArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
   const skeletonCards = twentyFiveArr.map((index) => <SkeletonCard key={`${index}-all-games-skeleton-card`}/>)
 
-  return (
+  // RENDER JSX ELEMENTS
+  if (wishlistLoading || !wishlistCards) return (
     <Grid w='100%' h='100%'>
       <Grid 
         w='100%'
@@ -146,10 +147,25 @@ export default function WishlistCards() {
         role='list'
         aria-label='Search results'
       >
-        {(wishlistLoading || !wishlistCards) && <>{skeletonCards}</>}
-        {wishlistError && <h1>{wishlistError}</h1>}
-        {wishlistCards && <>{wishlistCards}</>}
+        {skeletonCards}
       </Grid>
     </Grid>
   )
+
+  if (wishlistError) return <h1>{wishlistError}</h1>
+  if (wishlistCards && wishlistCards.length > 0) return (
+    <Grid w='100%' h='100%'>
+      <Grid 
+        w='100%'
+        h={!isSafari ? '100%' :'min-content'}
+        templateColumns='repeat(auto-fill, minmax(15rem, 1fr))'
+        gridGap='1.5rem'
+        role='list'
+        aria-label='Search results'
+      >
+        {wishlistCards}
+      </Grid>
+    </Grid>
+  )
+  if (wishlistCards.length === 0) return <>No cards found.</>
 }
