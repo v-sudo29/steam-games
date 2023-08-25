e220a280d32ad96983688ddcda859ace3841dff2
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-25 10:03:40 -0700

    Remove unused import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { DefaultDataProvider } from './context/defaultDataContext';
