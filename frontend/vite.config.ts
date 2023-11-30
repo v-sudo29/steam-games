3cf9d0a8d768ddf720d49cf4ba2ce62dd52e48e0
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-11-30 09:31:56 -0500

    Uncomment testing config in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,12 +9,12 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    css: true,
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './src/test/setup.ts',
+  //   // you might want to disable it, if you don't have tests that rely on CSS
+  //   // since parsing CSS is slow
+  //   css: true,
+  // },
 })
