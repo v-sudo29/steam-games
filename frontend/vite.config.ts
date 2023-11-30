7c533bb2b8f05ed2a28d3a6628aef0f0bafadb12
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-11-30 09:53:37 -0500

    Uncomment references and test option

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-// / <reference types="vitest" />
-// / <reference types="vite/client" />
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -9,12 +9,12 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  //   setupFiles: './src/test/setup.ts',
-  //   // you might want to disable it, if you don't have tests that rely on CSS
-  //   // since parsing CSS is slow
-  //   css: true,
-  // },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 })
