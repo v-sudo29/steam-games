b366384f8a319faab02871cf08e547c1464dacb8
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-11-30 09:29:38 -0500

    Update vite.config.ts imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,12 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
